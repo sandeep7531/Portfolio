3f8a37e5362770b3735d6c152b1cbe84e23fa85e
sandeep7531 <147388639+sandeep7531@users.noreply.github.com>
2024-02-17 15:14:37 +0530

    my portfolio

---
+++ constants/constants.js
@@ -0,0 +1,57 @@
+export const NAME = 'Sandeep';
+
+export const DESIGNATION = `Front-end Developer`;
+
+export const DETAILS = {
+  Residence: 'India',
+  City: 'New Delhi',
+  Age: '25',
+};
+
+export const SKILLS = [
+  {
+    title: 'React Js',
+    level: '91%',
+  },
+  {
+    title: 'Next Js',
+    level: '85%',
+  },
+  {
+    title: 'UI/UX Development',
+    level: '75%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'AWS',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'raisandeep756@gmail.com',
+  PHONE: '+917531954900',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/sandeep7531',
+  LINKEDIN: 'https://www.linkedin.com/in/sandeep-rai7531/',
+};
---
+++ pages/contact.jsx
@@ -0,0 +1,126 @@
+import { useState } from 'react';
+import BannerLayout from '../components/Common/BannerLayout';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import { SiUpwork } from 'react-icons/si'
+import { HiMail, HiUser } from 'react-icons/hi'
+import { BsChatTextFill } from 'react-icons/bs'
+import Fiverr_Icon from '../components/Fiverr_Icon';
+import Footer from '../components/Footer';
+import { Modal } from 'antd';
+import { CONTACTS, SOCIAL_LINKS } from '../constants/constants'
+import Link from 'next/link';
+
+const Contact = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <BannerLayout>
+            <div className=" px-4 py-2">
+                <div className="my-6 text-Snow flex flex-col gap-y-5">
+                    <h1 className='text-lg font-bold'>Contact Information</h1>
+                    <div className="flex flex-col md:flex-row items-center gap-5 text-xs">
+                        <div className="card_stylings w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
+                            <div className="flex justify-between items-center">
+                                <span className='md:text-base'>Country:</span>
+                                <span className='text-LightGray md:text-sm'>India</span>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <span className='md:text-base'>City:</span>
+                                <span className='text-LightGray md:text-sm'>New Delhi</span>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <span className='md:text-base'>Company:</span>
+                                <span className='text-LightGray md:text-sm'>Aurum Wisex</span>
+                            </div>
+                        </div>
+                        <div className="card_stylings rounded-xl w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
+                            <div className="flex justify-between items-center">
+                                <span className='md:text-base'>Email:</span>
+                                <Link href={`mailto:${CONTACTS.EMAIL}`}>
+                                    <span className='text-LightGray text-sm'>{CONTACTS.EMAIL}</span>
+                                </Link>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <span className='md:text-base'>Linkedin:</span>
+                                <Link href={SOCIAL_LINKS.LINKEDIN}>
+                                    <span className='text-LightGray text-sm'>sandeep-rai7531</span>
+                                </Link>
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <span className='md:text-base'>Phone:</span>
+                                <Link href={`tel:${CONTACTS.PHONE}`}>
+                                    <span className='text-LightGray text-sm'>{CONTACTS.PHONE}</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="h-16 w-full card_stylings text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items-center justify-center text-Snow">
+                    <a className='hover:scale-125 ease-in-out duration-700' href={`mailto:${CONTACTS.EMAIL}`} rel="noreferrer"><HiMail /></a>
+                    <a className='hover:scale-125 ease-in-out duration-700' href={SOCIAL_LINKS.GITHUB} target='_blank' rel="noreferrer"><FaGithub /></a>
+                    <a className='hover:scale-125 ease-in-out duration-700' href={SOCIAL_LINKS.LINKEDIN} target='_blank' rel="noreferrer"><FaLinkedin /></a>
+                    {/*<a className='hover:scale-125 ease-in-out duration-700 text-2xl sm:text-4xl mt-1' href="#" target='_blank' rel="noreferrer"><SiUpwork /></a>*/}
+                </div>
+
+
+                <div className="my-12 w-full h-auto text-Snow">
+                    <h1 className='text-lg font-bold'>Get In Touch</h1>
+                    <div className="mt-4 py-8 px-8 bg-EveningBlack rounded-xl text-sm">
+                        <div>
+                            <div className="flex flex-col w-full">
+                                <div className="userIcon relative mb-6">
+                                    <div id="icon" className="absolute inset-y-0 left-0 flex items-center pl-3 text-xl pointer-events-none">
+                                        <HiUser />
+                                    </div>
+                                    <input type="text" className="input_stylings" placeholder="Name" />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col w-full">
+                                <div className="mailIcon relative mb-6">
+                                    <div id="icon" className="absolute inset-y-0 left-0 flex items-center text-xl pl-3 pointer-events-none">
+                                        <HiMail />
+                                    </div>
+                                    <input type="text" className="input_stylings" placeholder="Email" />
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col w-full">
+                                <div className="textIcon relative mb-6">
+                                    <div id="icon" className="absolute top-3 left-0 flex items-center text-lg pl-3 pointer-events-none">
+                                        <BsChatTextFill />
+                                    </div>
+                                    <textarea rows={6} cols={50} className="input_stylings" placeholder="Message" />
+                                </div>
+                            </div>
+
+                            <div className="my-4">
+                                <button onClick={() => setIsOpen(true)} className="button"> SEND MESSAGE </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* success modal */}
+            <Modal
+                className='card_stylings backdrop-blur-3xl drop-shadow-2xl'
+                // wrapClassName='bg-red-800'
+                centered
+                open={isOpen}
+                footer={null}
+                closable={false}
+                onOk={() => setIsOpen(false)}
+                onCancel={() => setIsOpen(false)}
+            >
+                <div className='flex flex-col items-center justify-center'>
+                    <h1 className='text-Green font-bold text-2xl'>In Progress</h1>
+                    <a className='underline text-Snow' target='_blank' href={SOCIAL_LINKS.GITHUB}>Be the one to integrate this!</a>
+                </div>
+            </Modal>
+            <Footer />
+        </BannerLayout>
+
+    )
+}
+
+export default Contact
